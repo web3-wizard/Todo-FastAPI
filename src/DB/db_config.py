3a631ac5c3bfeecbd78155a0237385a7a61bf676
Rahul Dey <rahuldey9623@gmail.com>
2025-04-09 13:03:14 +0000

    Add Sqlite database config

---
+++ src/DB/db_config.py
@@ -0,0 +1,13 @@
+from sqlmodel import Session, SQLModel, create_engine
+
+sqlite_file_name = "DEV.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    return Session(engine)
