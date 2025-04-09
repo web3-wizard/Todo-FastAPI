0b4bb9244f56bd93ebf8f14e3186c98ac60db1ba
Rahul Dey <rahuldey9623@gmail.com>
2025-04-09 13:50:01 +0000

    Add iterations in test suite

--- tests/test_todo_api.py
+++ tests/test_todo_api.py
@@ -2,9 +2,11 @@ import httpx
 import pytest
 
 BASE_URL = "http://localhost:8000/api/todos"
+ITERATION = 10
+INVALID_ID = 2649
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("iteration", range(5))
+@pytest.mark.parametrize("iteration", range(ITERATION))
 async def test_create_todo(iteration):
     async with httpx.AsyncClient() as client:
         response = await client.post(f"{BASE_URL}/", json={"title": "Buy groceries", "completed": False})
@@ -16,7 +18,7 @@ async def test_create_todo(iteration):
         }
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("iteration", range(5))
+@pytest.mark.parametrize("iteration", range(ITERATION))
 async def test_get_all_todos(iteration):
     async with httpx.AsyncClient() as client:
         response = await client.get(f"{BASE_URL}/")
@@ -24,7 +26,7 @@ async def test_get_all_todos(iteration):
         assert isinstance(response.json(), list)
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("iteration", range(5))
+@pytest.mark.parametrize("iteration", range(ITERATION))
 async def test_get_todo_by_id(iteration):
     async with httpx.AsyncClient() as client:
         response = await client.get(f"{BASE_URL}/{iteration + 1}")
@@ -36,7 +38,7 @@ async def test_get_todo_by_id(iteration):
         }
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("iteration", range(5))
+@pytest.mark.parametrize("iteration", range(ITERATION))
 async def test_update_todo(iteration):
     async with httpx.AsyncClient() as client:
         response = await client.put(f"{BASE_URL}/{iteration + 1}", json={"title": "Buy groceries and cook dinner", "completed": True})
@@ -48,32 +50,35 @@ async def test_update_todo(iteration):
         }
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("iteration", range(5))
+@pytest.mark.parametrize("iteration", range(ITERATION))
 async def test_delete_todo(iteration):
     async with httpx.AsyncClient() as client:
         response = await client.delete(f"{BASE_URL}/{iteration + 1}")
         assert response.status_code == 204
 
 @pytest.mark.asyncio
-async def test_get_non_existent_todo():
+@pytest.mark.parametrize("iteration", range(ITERATION))
+async def test_get_non_existent_todo(iteration):
     async with httpx.AsyncClient() as client:
-        response = await client.get(f"{BASE_URL}/999")
+        response = await client.get(f"{BASE_URL}/{INVALID_ID + iteration }")
         assert response.status_code == 404
         assert response.json() == {
-            "detail": "Todo with id: 999 not found"
+            "detail": f"Todo with id: {INVALID_ID + iteration} not found"
         }
 
 @pytest.mark.asyncio
-async def test_create_todo_with_invalid_data():
+@pytest.mark.parametrize("iteration", range(ITERATION))
+async def test_create_todo_with_invalid_data(iteration):
     async with httpx.AsyncClient() as client:
         response = await client.post(f"{BASE_URL}/", json={"completed": False})
         assert response.status_code == 422
         assert "Field required" in response.json()["detail"][0]["msg"]
 
 @pytest.mark.asyncio
-async def test_get_todo_with_invalid_id():
+@pytest.mark.parametrize("iteration", range(ITERATION))
+async def test_get_todo_with_invalid_id(iteration):
     async with httpx.AsyncClient() as client:
-        response = await client.get(f"{BASE_URL}/-8")
+        response = await client.get(f"{BASE_URL}/-{iteration}")
         assert response.status_code == 422
         assert "Input should be greater than 0" in response.json()["detail"][0]["msg"]
 
